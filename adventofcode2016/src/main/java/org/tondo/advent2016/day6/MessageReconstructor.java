d0629c53d90f3e4e0856e07b6901f8e772873f2c
TondoDev <tondodeveloper@gmail.com>
2016-12-26 23:55:27 +0100

    Day 6 - Part 1

---
+++ adventofcode2016/src/main/java/org/tondo/advent2016/day6/MessageReconstructor.java
@@ -0,0 +1,63 @@
+package org.tondo.advent2016.day6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author TondoDev
+ *
+ */
+public class MessageReconstructor {
+	
+	private static final int MSG_LEN = 8;
+	private List<Map<Character, Integer>> charCounter;
+	
+	public MessageReconstructor() {
+		this.charCounter = new ArrayList<Map<Character, Integer>>();
+		for (int i = 0; i < MSG_LEN; i++) {
+			this.charCounter.add(new HashMap<Character, Integer>());
+		}
+	}
+	
+	
+	public void processMessageChunk(String chunk) {
+		int len = chunk.length();
+		for (int i = 0; i < len; i++) {
+			char c = chunk.charAt(i);
+			Map<Character, Integer> counter = this.charCounter.get(i);
+			Integer count = counter.get(c);
+			if (count == null) {
+				counter.put(c, 1);
+			} else {
+				counter.put(c, ++count);
+			}
+		}
+	}
+	
+	public String reconstruct() {
+		StringBuilder buffer = new StringBuilder();
+		
+		for (Map<Character, Integer> entry : this.charCounter) {
+			buffer.append(findMaxOccured(entry));
+		}
+		
+		return buffer.toString();
+	}
+	
+	private char findMaxOccured(Map<Character, Integer> chars) {
+		char maxChar = 0;
+		int maxOccurence = -1;
+		for (Map.Entry<Character, Integer> entry : chars.entrySet()) {
+			if (entry.getValue() > maxOccurence) {
+				maxChar = entry.getKey();
+				maxOccurence = entry.getValue();
+			}
+		}
+		
+		return maxChar;
+	}
+
+}
---
+++ adventofcode2016/src/test/java/org/tondo/advent2016/run/day6/Day6Test.java
@@ -0,0 +1,35 @@
+package org.tondo.advent2016.run.day6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import org.junit.Test;
+import org.tondo.advent2016.day6.MessageReconstructor;
+
+/**
+ * 
+ * @author TondoDev
+ *
+ */
+public class Day6Test {
+
+	@Test
+	public void testPart1() throws IOException {
+		
+		InputStream input = getClass().getResourceAsStream("/day6/day6Part1.txt");
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")))) {
+			MessageReconstructor recon = new MessageReconstructor();
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				if (!line.isEmpty()) {
+					recon.processMessageChunk(line);
+				}
+			}
+			
+			System.out.println("Day 6 Part 1: message " + recon.reconstruct());
+		}
+	}
+}
