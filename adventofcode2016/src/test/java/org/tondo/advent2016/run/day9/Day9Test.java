7c080aa7caf75d6feb6c9f27e9426db0f06f9620
Tondo at work <tondo@work.com>
2017-01-13 09:18:44 +0100

    Day 9 - Assignment structure prepared

---
+++ adventofcode2016/src/test/java/org/tondo/advent2016/run/day9/Day9Test.java
@@ -0,0 +1,25 @@
+package org.tondo.advent2016.run.day9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author TondoDev
+ *
+ */
+public class Day9Test {
+	
+	@Test
+	public void testPart1() throws UnsupportedEncodingException, IOException {
+		InputStream input = getClass().getResourceAsStream("/day9/day9Part1.txt");
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
+			
+		}
+	}
+}
