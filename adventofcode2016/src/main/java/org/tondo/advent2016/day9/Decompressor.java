867b70b034e0e1bee2999610256af9d05ae73e5d
Tondo at work <tondo@work.com>
2017-01-13 15:30:54 +0100

    Day 9  - part 1

---
+++ adventofcode2016/src/main/java/org/tondo/advent2016/day9/Decompressor.java
@@ -0,0 +1,136 @@
+package org.tondo.advent2016.day9;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * 
+ *  @author TondoDev
+ */
+public class Decompressor {
+	
+	private enum MarkState {
+		STARTF,
+		FIRST,
+		STARTS,
+		SECOND
+	}
+	
+	private static class Mark {
+		private int charsCount;
+		private int repeatCount;
+		private int readedData;
+		final boolean ok;
+		
+		public Mark(int cc, int rc) {
+			this.charsCount = cc;
+			this.repeatCount = rc;
+			this.readedData = 0;
+			ok = true;
+		}
+		
+		public Mark(int readed) {
+			this.readedData = readed;
+			ok = false;
+		}
+		
+		public boolean isOk() {
+			return ok;
+		}
+		
+		public int getCharsCount() {
+			return charsCount;
+		}
+		public int getRepeatCount() {
+			return repeatCount;
+		}
+		
+		public int getReadedData() {
+			return readedData;
+		}
+	}
+
+	private long decompressedLen = 0;
+	
+	public void decompress(Reader reader) throws IOException {
+		
+		int readChar = 0;
+		while((readChar = reader.read()) != -1) {
+			char c = (char) readChar;
+			
+			if (c == '(') {
+				Mark mark = readComressionMark(reader);
+				if (mark.isOk()) {
+					this.decompressedLen += expend(mark, reader);
+				} else {
+					// incorrect syntax in mark is treated as standard data
+					// count also opening bracket.
+					this.decompressedLen += (mark.getReadedData() + 1);
+				}
+			} else if (!Character.isWhitespace(c)) {
+				// ignoring whitespace
+				this.decompressedLen++;
+			}
+		}
+	}
+	
+	private Mark readComressionMark(Reader reader) throws IOException {
+		StringBuilder token = new StringBuilder();
+		
+		MarkState state = MarkState.STARTF;
+		int data = 0;
+		int readedCnt = 0;
+		int chars = 0;
+		int reps = 0;
+		while ((data = reader.read()) != -1) {
+			readedCnt++;
+			char c = (char)data;
+			
+			if (Character.isDigit(c) && (state == MarkState.STARTF || state == MarkState.FIRST)) {
+				state = MarkState.FIRST;
+				token.append(c);
+			} else if (c == 'x' && state == MarkState.FIRST) {
+				try {
+					chars = Integer.valueOf(token.toString()); 
+					token = new StringBuilder();
+				} catch (NumberFormatException e) {
+					return new Mark(readedCnt);
+				}
+				state = MarkState.STARTS;
+			} else if (Character.isDigit(c) && (state == MarkState.STARTS || state == MarkState.SECOND)) {
+				state = MarkState.SECOND;
+				token.append(c);
+			} else if (c == ')' && state == MarkState.SECOND) {
+				try {
+					reps = Integer.valueOf(token.toString()); 
+					return new Mark(chars, reps);
+				} catch (NumberFormatException e) {
+					return new Mark(readedCnt);
+				}
+			} else {
+				return new Mark(readedCnt);
+			}
+		}
+		
+		return new Mark(readedCnt);
+	}
+	
+	
+	private int expend(Mark compressMark, Reader reader) throws IOException {
+		if (compressMark.getCharsCount() == 0) {
+			return 0;
+		}
+		
+		int readed = 0;
+		while (readed < compressMark.getCharsCount() && reader.read() != -1) {
+			readed++;
+		}
+		
+		return readed * compressMark.getRepeatCount();
+	}
+	
+	
+	public long getDecompressedLen() {
+		return decompressedLen;
+	}
+}
--- adventofcode2016/src/test/java/org/tondo/advent2016/run/day9/Day9Test.java
+++ adventofcode2016/src/test/java/org/tondo/advent2016/run/day9/Day9Test.java
@@ -1,12 +1,16 @@
 package org.tondo.advent2016.run.day9;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
+import org.tondo.advent2016.day9.Decompressor;
 
 /**
  * 
@@ -19,7 +23,56 @@ public class Day9Test {
 	public void testPart1() throws UnsupportedEncodingException, IOException {
 		InputStream input = getClass().getResourceAsStream("/day9/day9Part1.txt");
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
+			Decompressor decompressor = new Decompressor();
+			decompressor.decompress(reader);
 			
+			System.out.println("Day 9 Part 1: Decomressed length: " + decompressor.getDecompressedLen()); // 152851
 		}
+	}
+	
+	
+	@Test
+	public void testDecompressor() throws IOException {
+		Decompressor dec = null;
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader(""));
+		assertEquals("empty string", 0, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("ABCD"));
+		assertEquals(4, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("ABCD("));
+		assertEquals("incomplete mark", 5, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("ABCD(58"));
+		assertEquals("incomplete mark", 7, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("ABCD(58a58)"));
+		assertEquals("wrong syntax mark", 11, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("A(1x5)BC"));
+		assertEquals(7, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("(3x3)XYZ"));
+		assertEquals(9, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("A(2x2)BCD(2x2)EFG"));
+		assertEquals(11, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("(6x1)(1x3)A"));
+		assertEquals(6, dec.getDecompressedLen());
+		
+		dec = new Decompressor();
+		dec.decompress(new StringReader("X(8x2)(3x3)ABCY"));
+		assertEquals(18, dec.getDecompressedLen());
 	}
 }
