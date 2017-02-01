7a5d5cda7fcf55a74207d6fb6b426d36515d788e
TondoDev <tondodeveloper@gmail.com>
2017-02-01 23:00:51 +0100

    Day 11 - state exloration prototype

--- adventofcode2016/src/main/java/org/tondo/advent2016/day11/StateSpace.java
+++ adventofcode2016/src/main/java/org/tondo/advent2016/day11/StateSpace.java
@@ -1,8 +1,10 @@
 package org.tondo.advent2016.day11;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -10,6 +12,7 @@ import java.util.List;
  *
  */
 public class StateSpace {
+	private static final int ELEVATOR_CAPACITY = 2;
 	
 	private static class InternalState {
 		int steps;
@@ -24,7 +27,6 @@ public class StateSpace {
 	}
 	
 	private FloorState initialState;
-	private FloorState endState;
 	private LinkedList<InternalState> fifo;
 	private LinkedList<FloorState> minimalPath;
 	
@@ -63,17 +65,60 @@ public class StateSpace {
 	private List<InternalState> generateNextStates(InternalState currState) {
 		List<InternalState> rv = new ArrayList<>();
 		
-		if (currState.curr.getElevatorFloor() < 4) {
-			int nextElevator = currState.curr.getElevatorFloor() + 1;
+		List<String> curretnFloorState = currState.curr.getFloors().get(currState.curr.getElevatorFloor());
+		SubsetGen stateGen = new SubsetGen(curretnFloorState, ELEVATOR_CAPACITY);
+		
+		while (stateGen.hasNext()) {
+			List<String> state = stateGen.getNext();
+			List<String> nextSourceFloorState = new ArrayList<>(curretnFloorState);
+			nextSourceFloorState.removeAll(state);
+			
+			if (!isValidFloorConfiguration(nextSourceFloorState)) {
+				continue;
+			}
+			
+			for (int direction : new int[] {-1, 1}) {
+				int nextFloor = currState.curr.getElevatorFloor() + direction;
+				if (nextFloor <= 4 && nextFloor >= 0) {
+					List<String> currentTargetFloorState = currState.curr.getFloors().get(nextFloor);
+					List<String> nextTargetFloorState = new ArrayList<>(currentTargetFloorState);
+					nextTargetFloorState.addAll(state);
+					if (isValidFloorConfiguration(nextTargetFloorState)) {
+						Map<Integer, List<String>> nextFloors = new HashMap<>(currState.curr.getFloors());
+						nextFloors.put(nextFloor, nextTargetFloorState);
+						nextFloors.put(currState.curr.getElevatorFloor(), nextSourceFloorState);
+						FloorState fs = new FloorState(nextFloors, nextFloor);
+						if (!fs.equals(currState.prev)) {
+							rv.add(new InternalState(fs, currState, currState.steps+1));
+						}
+					}
+				}
+			}
 		}
-		
-		if (currState.curr.getElevatorFloor() > 0) {
-			int nextElevator = currState.curr.getElevatorFloor() - 1;
-		}
-		
-		
-		
 		return rv;
+	}
+	
+	private boolean isValidFloorConfiguration(List<String> devices) {
+		for (String dev : devices) {
+			char element = dev.charAt(0);
+			char deviceType = dev.charAt(1);
+			
+			if (deviceType == 'm' && isChipInDanger(element, devices)) {
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	private boolean isChipInDanger(char elem, List<String> devices) {
+		for (String dev : devices) {
+			if (dev.charAt(1) == 'g' && dev.charAt(0) != elem) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	private void constructMinPath(InternalState state, LinkedList<FloorState> target) {
