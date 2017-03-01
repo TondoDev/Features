4bbf1ff857178db8afbcec7ed4eb9a2a8a8cf254
Tondo at work <tondo@work.com>
2017-03-01 10:30:29 +0100

    Day 15 - created task structure

---
+++ adventofcode2016/src/test/java/org/tondo/advent2016/run/day15/Day15Test.java
@@ -0,0 +1,25 @@
+package org.tondo.advent2016.run.day15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author TondoDev
+ *
+ */
+public class Day15Test {
+
+	@Test
+	public void testPart1() throws UnsupportedEncodingException, IOException {
+		InputStream input = getClass().getResourceAsStream("/day15/day15Part1.txt");
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
+		
+		}
+	}
+}
