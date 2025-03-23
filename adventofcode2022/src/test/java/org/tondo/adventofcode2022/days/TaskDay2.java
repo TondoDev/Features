33926510d217984f0ee8627c0c8b1ac5bf49b93c
TondoDev <tondodeveloper@gmail.com>
2025-03-23 13:46:27 +0100

    [aoc2022] some imports

--- adventofcode2022/src/test/java/org/tondo/adventofcode2022/days/TaskDay2.java
+++ adventofcode2022/src/test/java/org/tondo/adventofcode2022/days/TaskDay2.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
